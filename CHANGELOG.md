8a6938e143c58fbc77fbe757c380773b43a55187
Phil Johnston <phil.johnston@wpengine.com>
2024-01-22 13:23:01 -0500

    Fix issues with prettier in wp-scripts 27

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,10 @@
 - wp-scripts version updated to 27
 - @wordpress/jest-preset-default updated to "^11.20.0"
 - @wordpress/eslint-plugin updated to "^17.6.0"
+- @wordpress/prettier-config package added to pluginade
+- Prettier package set to use WordPress fork of prettier: wp-prettier
+- .eslintrc files located in plugin root directories are now copied into .pluginade, temporarily renamed to .eslintrc-temp and fixed once lint jobs are complete.
+- lint:js and lint:js:fix now default to use the config from @wordpress/scripts/config/.eslintrc.js, but can be overriden by a plugin with a custom .eslintrc file in the plugin root.
 
 ### Fixed
 - Sometimes `dev` and `build` could fail depending on the order of commands you've run. Now, we run npm install for wp-modules even if node_modules are empty. This can happen because we mount empty node_modules docker volumes to speed up some workflows like phpunit, but if npm install hasn't been run yet, node_modules doesn't even exist, so an empty directory was created. Now `dev` and `build` check if those directories are empty.
--- lint-js.sh
+++ lint-js.sh
@@ -6,15 +6,26 @@
 # Check if there is an .eslintrc file in this project. If so, use it for standards. Otherwise,
 if [ -f "$plugindir/.eslintrc" ];
 then
-	if [ "$fix" = "1" ]; then
-		npm run lint:js "$plugindir" -- --config $plugindir/.eslintrc --fix;
-	else
-		npm run lint:js "$plugindir" -- --config $plugindir/.eslintrc;
-	fi
+	# Copy the .eslintrc file from the plugin to pluginade.
+	cp "$plugindir/.eslintrc" ".eslintrc";
+
+	# Temporarily rename the .eslintrc file in the plugin to avoid conflicts.
+	mv "$plugindir/.eslintrc" "$plugindir/.eslintrc-temp";
+	eslintFileName=".eslintrc";
 else 
-	if [ "$fix" = "1" ]; then
-		npm run lint:js "$plugindir" -- --config .eslintrc --fix;
-	else
-		npm run lint:js "$plugindir" -- --config .eslintrc;
-	fi
+	# Copy the .eslintrc file WP scripts in node_modules to pluginade.
+	cp ./node_modules/@wordpress/scripts/config/.eslintrc.js ".eslintrc.js";
+	eslintFileName=".eslintrc.js";
+fi
+
+if [ "$fix" = "1" ]; then
+	npm run lint:js "$plugindir" -- --config $eslintFileName --fix;
+else
+	npm run lint:js "$plugindir" -- --config $eslintFileName;
+fi
+
+# Reset the .eslintrc file in the plugin.
+if [ -f "$plugindir/.eslintrc-temp" ];
+then
+	mv "$plugindir/.eslintrc-temp" "$plugindir/.eslintrc";
 fi
