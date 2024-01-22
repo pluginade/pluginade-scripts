8a2aea6855854e080fb61c7b30d0f126cd43c94a
Phil Johnston <phil.johnston@wpengine.com>
2024-01-22 16:29:23 -0500

    Fix exit code for CSS if no CSS files found

--- lint-css.sh
+++ lint-css.sh
@@ -2,7 +2,20 @@
 
 # Run setup.
 . ./setup.sh
-. ./install-npm.sh
+
+# Find CSS files and run stylelint if any are found
+files=$(find "$plugindir" -type f -name "*.*css")
+
+if [ -z "$files" ]; then
+	echo "No CSS files found in $plugindir. Skipping stylelint."
+	exit 0;
+fi
+
+# Install pluginade npm dependencies.
+if [ ! -d node_modules ] || [ -z "$(ls -A "$DIR/node_modules")" ]; then
+	echo "Running npm install in pluginade root at $DIR..."
+	npm install
+fi
 
 # Run the lint command from the wp-content directory.
 if [ "$fix" = "1" ]; then
