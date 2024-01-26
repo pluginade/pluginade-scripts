fd431f97a943942f185a763c89a8f3d697290888
Phil Johnston <phil.johnston@wpengine.com>
2024-01-26 15:42:07 -0500

    Use a boiler jest config

--- test-js.sh
+++ test-js.sh
@@ -4,4 +4,18 @@
 . ./setup.sh
 . ./install-npm.sh
 
-npx wp-scripts test-unit-js --passWithNoTests --roots "$plugindir"
+# Check if there is an jest.config.js file in this project. If so, use it for standards. Otherwise,
+if [ -f "$plugindir/jest.config.js" ];
+then
+	# Copy the jest.config.js file from the plugin to pluginade.
+	cp "$plugindir/jest.config.js" "jest.config.js";
+
+	# Temporarily rename the jest.config.js file in the plugin to avoid conflicts.
+	mv "$plugindir/jest.config.js" "$plugindir/jest.config.js-temp";
+	eslintFileName="jest.config.js";
+else 
+	# Use the jest.config.js file from pluginade.
+	cp "$plugindir/jest.config.js.boiler" "jest.config.js";
+fi
+
+npx wp-scripts test-unit-js --passWithNoTests --config  --roots "$plugindir/wp-modules/app/"
